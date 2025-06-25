e90270c4c84cc6e41cdcf7188cacec1e36d308b5
Ustimenko Konstantin <ustimenko.k@gmail.com>
2025-06-25 10:04:46 +0300

    first-iteration

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,34 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func AutoDetectAndConvert(data string) (string, error) {
+	if data == "" {
+		return "", errors.New("input data is empty")
+	}
+
+	trimmedData := strings.TrimSpace(data)
+
+	isMorse := true
+	for _, char := range trimmedData {
+		if char != '.' && char != '-' && char != ' ' && char != '\n' && char != '\r' {
+			isMorse = false
+			break
+		}
+	}
+
+	if !strings.ContainsAny(trimmedData, ".-") {
+		isMorse = false
+	}
+
+	if isMorse {
+		return morse.ToText(trimmedData), nil
+	} else {
+		return morse.ToMorse(trimmedData), nil
+	}
+}
